cd262b380fd4ad124fe3dc0767bb2188b6196766
Erand Kurtaliqi <Erand Kurtaliqi@DESKTOP-2NPHQQH>
2026-01-04 22:14:17 +0100

    update

--- ml-kem-frontend/src/environments/environment.ts
+++ ml-kem-frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7123/api/PostQuantumCrypto'
+  // apiUrl: 'https://localhost:7123/api/PostQuantumCrypto'
+  apiUrl: 'https://ml-kem-apis.onrender.com/api/PostQuantumCrypto'
 };
 
